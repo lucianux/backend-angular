6d0f5ddc368d8f023a0b5c63506525ff482d328e
Chuzas <lucianux@gmail.com>
2020-09-15 00:22:12 -0300

    Agregado de mi copyright en el footer

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   template: `
     <span class="created-by">
-      Created with ♥ by <b><a href="https://akveo.page.link/8V2f" target="_blank">Akveo</a></b> 2019
+      Created with ♥ by <b><a href="http://www.chusoft.com" target="_blank">Chusoft</a></b> {{ anio }}
     </span>
     <div class="socials">
       <a href="#" target="_blank" class="ion ion-social-github"></a>
@@ -16,4 +16,5 @@ import { Component } from '@angular/core';
   `,
 })
 export class FooterComponent {
+  anio: number = new Date().getFullYear();
 }
