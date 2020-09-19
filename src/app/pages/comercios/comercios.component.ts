53e0a124d8ba594b53eeb101f89dff07f70b937d
Chuzas <lucianux@gmail.com>
2020-09-19 16:43:51 -0300

    Instalación de Firebase y conexión con la DB.
    Se instaló Firebase.
    Trae datos de la DB y los muestra en pantalla.

--- src/app/pages/comercios/comercios.component.ts
+++ src/app/pages/comercios/comercios.component.ts
@@ -1,35 +1,36 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ComercioService } from '../../services/comercio.service';
+import { Comercio } from '../../model/comercio';
 
 @Component({
   selector: 'ngx-comercios-elements',
   templateUrl: 'comercios.component.html',
   styleUrls: ['comercios.component.scss'],
-  providers: [ ComercioService ]
+  providers: [ComercioService]
 })
 export class ComerciosComponent {
 
-    fruits: string[] = [
-        'il Tano - Pizzas',
-        'Pollería - Don Torcuato',
-        'Heladería - Iglú'
-    ];
+  comercios: Array<Comercio>;
 
-    constructor(
-      private router: Router,
-      private comercioSer: ComercioService)
-    {
-      this.fruits = this.comercioSer.obtenerComercios();
-    }
+  constructor(
+    private router: Router,
+    private comercioSer: ComercioService)
+  {
+    this.comercios = new Array<Comercio>();
+    this.comercioSer.obtenerComercios().subscribe(
+      (data: Array<Comercio>) => {
+        this.comercios = data;
+      },
+      err => console.log(err)
+    );
+  }
 
-    public goToAgregar()
-    {
-      this.router.navigate(['/pages/agregar']);
-    }
+  public goToAgregar() {
+    this.router.navigate(['/pages/agregar']);
+  }
 
-    public goToEditar()
-    {
-      this.router.navigate(['/pages/editar']);
-    }
+  public goToEditar() {
+    this.router.navigate(['/pages/editar']);
+  }
 }
--- src/app/services/comercio.service.ts
+++ src/app/services/comercio.service.ts
@@ -1,18 +1,52 @@
 import { Injectable } from "@angular/core";
+import {
+    AngularFirestore,
+    AngularFirestoreCollection,
+    AngularFirestoreDocument }
+        from '@angular/fire/firestore';
+import { map, take, first } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { Comercio } from '../model/comercio';
 
 @Injectable()
 export class ComercioService {
-    private comercios: string[];
+    private comercios_string: string[];
+    comercios: Observable<Comercio[]>;
+    private coleccionDeComercios: AngularFirestoreCollection<Comercio>;
 
-    constructor() {
-        this.comercios = ['Com1', 'Com2', 'Com3'];
+    constructor(private afs: AngularFirestore) {
+        this.comercios_string = ['Com1', 'Com2', 'Com3'];
+
+        this.coleccionDeComercios = afs.collection<Comercio>('comercios');
+        //this.comercios = this.coleccionDeComercios.valueChanges();
+        this.comercios = this.coleccionDeComercios.snapshotChanges().pipe(
+            map(actions => actions.map(a => {
+                const data = a.payload.doc.data() as Comercio;
+                const id = a.payload.doc.id;
+                return { id, ...data };
+            }))
+        );
     }
 
-    public obtenerComercios() {
+    // public obtenerComercios(): Observable<Comercio[]> {
+    //     return this.comercios;
+    // }
+
+    public publicarComercio(comercio: Comercio) {
+        this.coleccionDeComercios.add(comercio);
+    }
+
+    public actualizarComercio(comercio: Comercio) {
+        var comercioActualizable: AngularFirestoreDocument;
+        comercioActualizable = this.afs.doc<Comercio>(`comercios/${comercio.id}`);
+        comercioActualizable.update(comercio);
+    }
+
+    public obtenerComercios(): Observable<Comercio[]> {
         return this.comercios;
     }
 
     public agregarComercio(comercio: string) {
-        this.comercios.push(comercio);
+        this.comercios_string.push(comercio);
     }
 }
